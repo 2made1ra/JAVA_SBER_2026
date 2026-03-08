c934c88cfa3a6f3bb09c19ce619ff65302ed812b
Andrey Zhunev <im2made1ra@gmail.com>
2026-03-08 16:53:56 +0500

    feat(lab): выполнены 5 тасков из лабораторной 2

---
+++ java-core-2026/src/LAB_2/tasks/Task4/Person.java
@@ -0,0 +1,57 @@
+package LAB_2.tasks.Task4;
+
+/**
+ * Создайте класс Person, который содержит поля для имени,
+ * возраста и пола человека.
+ * Добавьте методы, которые позволяют установить и получить
+ * значения этих полей.
+ * Добавьте конструктор, который позволяет создавать объекты
+ * класса Person, используя значения для имени, возраста и пола.
+ */
+public class Person {
+    // Поля класса
+    private String name;
+    private int age;
+    private String gender;
+
+    // Конструктор класса
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    // Геттер для name
+    public String getName() {
+        return this.name;
+    }
+
+    // Сеттер для name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Геттер для age
+    public int getAge() {
+        return this.age;
+    }
+
+    // Сеттер для age
+    public void setAge(int age) {
+        if (age >= 0 && age < 125) {
+            this.age = age;
+        } else {
+            System.out.println("Incorrect age!");
+        }
+    }
+
+    // Геттер для gender
+    public String getGender() {
+        return this.gender;
+    }
+
+    //Сеттер для gender
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ java-core-2026/src/LAB_2/tasks/Task5/Task5.java
@@ -0,0 +1,11 @@
+package LAB_2.tasks.Task5;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Rectangle testRect = new Rectangle(15.0, 10.0);
+        System.out.printf("Длина прямоугольника: %.2f\n", testRect.getLength());
+        System.out.printf("Ширина прямоугольника: %.2f\n", testRect.getWidth());
+        System.out.printf("Площадь прямоугольника: %.2f\n", testRect.area());
+        System.out.printf("Периметр прямоугольника: %.2f\n", testRect.perimeter());
+    }
+}
