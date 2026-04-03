fbf5bc51789961ac336b93a250a7cb7ce2ebadc7
Andrey Zhunev <im2made1ra@gmail.com>
2026-04-03 19:26:33 +0500

    feat(lab3): закончен класс для создания односвязного списка с помощью цикла

--- java-core-2026/src/lab3/tasks/task8/CycledLinkedList.java
+++ java-core-2026/src/lab3/tasks/task8/CycledLinkedList.java
@@ -8,21 +8,21 @@ import lab3.tasks.task4.Node;
  */
 public class CycledLinkedList {
     private Node head;
-    private int length;
+    private final int length;
 
     private CycledLinkedList (int length) {
         this.length = length;
         this.head = null;
     }
 
-    public static CycledLinkedList createWithHead(int n) {
-        CycledLinkedList list = new CycledLinkedList(n);
+    public static CycledLinkedList createWithHead(int length) {
+        CycledLinkedList list = new CycledLinkedList(length);
         list.createHead();
         return list;
     }
 
-    public static CycledLinkedList createWithTail(int n) {
-        CycledLinkedList list = new CycledLinkedList(n);
+    public static CycledLinkedList createWithTail(int length) {
+        CycledLinkedList list = new CycledLinkedList(length);
         list.createTail();
         return list;
     }
@@ -66,27 +66,83 @@ public class CycledLinkedList {
         return stringList.toString();
     }
 
-    public void addFirst() {
-
+    public void addFirst(int value) {
+        head = new Node(value, head);
     }
 
-    public void addLas() {
+    public void addLast(int value) {
+        if (head == null) {
+            System.out.println("Список пуст");
+            return;
+        }
 
+        Node newNode = new Node(value, null);
+        Node ref = head;
+
+        while (ref.next != null) {
+            ref = ref.next;
+        }
+
+        ref.next = newNode;
     }
 
-    public void insert() {
+    public void insert(int insertIndex, int value) {
+        if (insertIndex == 0) {
+            this.addFirst(value);
+            return;
+        }
 
+        Node newNode = new Node(value, null);
+        Node ref = head;
+        int index = 0;
+
+        while (index < insertIndex - 1) {
+            ref = ref.next;
+            index++;
+        }
+
+        newNode.next = ref.next;
+        ref.next = newNode;
     }
 
     public void removeFirst() {
-
+        Node ref = head;
+        head = ref.next;
     }
 
     public void removeLast() {
+        if (head == null || head.next == null) {
+            head = null;
+            return;
+        }
 
+        Node ref = head;
+        while (ref.next.next != null) {
+            ref = ref.next;
+        }
+        ref.next = null;
     }
 
-    public void remove() {
+    public void remove(int deleteIndex) {
+        if (head == null) {
+            System.out.println("Список пуст");
+            return;
+        } else if (deleteIndex == 0) {
+            head = head.next;
+            return;
+        } else if (deleteIndex > length - 1) {
+            System.out.println("Некорректный индекс");
+            return;
+        }
 
+        int index = 0;
+        Node ref = head;
+
+        while (index < deleteIndex - 1) {
+            ref = ref.next;
+            index++;
+        }
+
+        ref.next = ref.next.next;
     }
 }
